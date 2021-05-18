5eb4a646e3a2b5e14782155a453a9c650d6b75d3
okkjoo <keyjo_zhou@m.scnu.edu.cn>
2021-05-18 17:06:29 +0800

    驾驶飞船左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,35 +1,22 @@
 import pygame
-from pygame.sprite import Group
-
 from settings import Settings
 from ship import Ship
 import game_functions as gf
-
 def run_game():
-    # Initialize pygame, settings, and screen object.
-    # 初始化游戏并创建一个屏幕对象
+    #初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
-    # display.set_mode()返回的surface表示整个游戏窗口
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))   
     pygame.display.set_caption("Alien Invasion")
-    
-    # Set the background color.
-    bg_color = (230, 230, 230)
-    
-    # Make a ship.
-    ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
-    bullets = Group()
-
-    # Start the main loop for the game.
-    # 开始游戏的主循环
+    #创建一艘飞船
+    ship = Ship(ai_settings,screen)
+    #设置背景色
+    # bg_color =(230,230,230)#浅灰
+    #开始游戏的主循环
     while True:
-        # 监视鼠标和键盘事件
-        gf.check_events(ai_settings, screen, ship, bullets)
+        #监视键盘和鼠标事件
+        gf.check_events(ship)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
-
+        #每次循环时都重新绘制屏幕 让最近的屏幕绘制可见
+        gf.update_screen(ai_settings,screen,ship)
 run_game()
